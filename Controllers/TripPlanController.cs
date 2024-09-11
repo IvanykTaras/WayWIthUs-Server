4622cdf9fdf212a2bc3041021c5165091dfd3a3c
TarasIvanyk2002 <tarasivanyk001@gmail.com>
2024-09-11 23:16:55 +0200

    commit

--- Controllers/TripPlanController.cs
+++ Controllers/TripPlanController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using GoogleApi.Entities.Translate.Common.Enums;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using WayWIthUs_Server.Data;
 using WayWIthUs_Server.Entities;
+using WayWIthUs_Server.Service;
 
 namespace WayWIthUs_Server.Controllers
 {
@@ -12,10 +14,12 @@ namespace WayWIthUs_Server.Controllers
     public class TripPlanController : ControllerBase
     {
         private readonly IMongoCollection<TripPlan> _tripPlan;
-        public TripPlanController(MongoDbService mongoDbService, IConfiguration configuration)
+        private readonly IGooglePlacesService _googlePlacesService;
+        public TripPlanController(MongoDbService mongoDbService, IConfiguration configuration, IGooglePlacesService googlePlacesService)
         {
             var tripPlanCollectionName = configuration.GetConnectionString("TripPlanCollection");
             _tripPlan = mongoDbService.Database.GetCollection<TripPlan>(tripPlanCollectionName);
+            _googlePlacesService = googlePlacesService;
         }
 
         [HttpGet]
@@ -45,6 +49,20 @@ namespace WayWIthUs_Server.Controllers
             {
                 return BadRequest(ModelState);
             }
+
+            foreach (var hotel in tp.Hotels)
+            {
+                hotel.image_url = (await _googlePlacesService.getPhotoUrls(hotel.name, 400, 400)).FirstOrDefault();
+            }
+
+            foreach (var day in tp.Itinerary)
+            {
+                foreach (var place in day.Places)
+                {
+                    place.image_url = (await _googlePlacesService.getPhotoUrls(place.location, 400, 400)).FirstOrDefault();
+                }
+            }
+
 
             await _tripPlan.InsertOneAsync(tp);
             return CreatedAtAction(nameof(GetById), new { id = tp.Id }, tp);
