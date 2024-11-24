b10475dd8ee6fb122753c834549331843fd80198
IvanykTaras <hanekava4@gmail.com>
2024-11-24 22:00:24 +0100

    fix: api fix

--- Models/ApiKeyAndUrl.cs
+++ Models/ApiKeyAndUrl.cs
@@ -2,7 +2,7 @@
 {
     public static class ApiKeyAndUrl
     {
-        public static readonly string PLACES_API_KEY = "AIzaSyAaeF8LsmkU_Al-0NJ0Isg2BWvNOTrSxvY";
+        public static readonly string PLACES_API_KEY = "AIzaSyBPcSKuwLsx_Jr4npHMzWpKFhupBms7xfo";
         public static readonly string GEMINI_API_KEY = "AIzaSyBlCJARSX2-4UG1iKEQW2cg29yYjjmJjc0";
         public static readonly string CLIEND_ID_OAUTH = "855465720082-amiqkfaec83mnj7ttmjagp3ueavhqqr7.apps.googleusercontent.com";
         public static readonly string USER_INFO = "https://www.googleapis.com/oauth2/v2/userinfo?access_token={token}";
