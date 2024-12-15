288fd1970b0bb780b918d744a9394e3709b8bcab
IvanykTaras <hanekava4@gmail.com>
2024-12-15 01:03:09 +0100

    ai

--- Controllers/TripPlanController.cs
+++ Controllers/TripPlanController.cs
@@ -32,9 +32,9 @@ namespace WayWIthUs_Server.Controllers
             var tripPlanCollectionName = configuration.GetConnectionString("TripPlanCollection");
             _tripPlan = mongoDbService.Database.GetCollection<TripPlan>(tripPlanCollectionName);
 
-            var userCollectionName = configuration.GetConnectionString("UserCollection");
-            _user = mongoDbService.UserDatabase.GetCollection<User>(userCollectionName);
-
+            var users = configuration.GetConnectionString("UserCollection");
+            _user = mongoDbService.UserDatabase.GetCollection<User>(users);
+            
             _googlePlacesService = googlePlacesService;
 
             _openAiService = openAiService;
@@ -106,32 +106,7 @@ namespace WayWIthUs_Server.Controllers
                 return BadRequest(ModelState);
             }
 
-
-            foreach (var city in tp.CityPlans)
-            {
-                city.Image_url = (await _googlePlacesService.getPhotoUrls(city.OriginLocation, 400, 400)).FirstOrDefault();
-            }
-
-            foreach (var city in tp.CityPlans)
-            {
-                foreach (var accomodation in city.Accommodations)
-                {
-                    accomodation.image_url = (await _googlePlacesService.getPhotoUrls(accomodation.location_acc, 400, 400)).FirstOrDefault();
-                    accomodation.googleMapUrl = await _googlePlacesService.getPlaceLink(accomodation.location_acc);
-                }
-            }
-
-            foreach (var city in tp.CityPlans)
-            {
-                foreach (var place in city.Places)
-                {
-                    place.googleMapUrl = await _googlePlacesService.getPlaceLink(place.location);
-                    place.image_url = (await _googlePlacesService.getPhotoUrls(place.location, 400, 400)).FirstOrDefault();
-                }
-            }
-
-
-            var filter = Builders<TripPlan>.Filter.Eq(e => e.Id, id); 
+            var filter = Builders<TripPlan>.Filter.Eq(e => e.Id, id);
             var updateResult = await _tripPlan.ReplaceOneAsync(filter, tp);
 
             if (updateResult.IsAcknowledged && updateResult.ModifiedCount > 0)
@@ -187,23 +162,25 @@ namespace WayWIthUs_Server.Controllers
             return Ok();
         }
 
-        [AllowAnonymous]
-        [HttpGet("{tripId}/participants")]
-        public async Task<ActionResult> Participants(string tripId)
+        [HttpPost("{tripId}/participants")]
+        public async Task<IActionResult> GetParticipants(string tripId)
         {
+            var filter = Builders<TripPlan>.Filter.Eq(e => e.Id, tripId);
+            var tripPlan = await _tripPlan.Find(filter).FirstOrDefaultAsync();
 
-            List<User> users = new List<User>();
+            List<User> participants = new List<User>();
 
-            var tripPlan = await _tripPlan.Find(t => t.Id == tripId).FirstOrDefaultAsync(); 
-
-            foreach (var userId in tripPlan.Participants)
+            foreach (var participantId in tripPlan.Participants)
             {
-                var user = await _user.Find(u => u.Id == userId).FirstOrDefaultAsync();
-                users.Add(user);
+                var participantFilter = Builders<User>.Filter.Eq(e => e.Id, participantId);
+                var participant = await _user.Find(participantFilter).FirstOrDefaultAsync();
+                participants.Add(participant);
             }
 
-            return Ok(users);
+            return Ok(participants);
         }
+
+
 
         [AllowAnonymous]
         [HttpPost("/getOpenAiResponse")]
@@ -213,8 +190,7 @@ namespace WayWIthUs_Server.Controllers
             return Ok(response);
         }
 
-        [AllowAnonymous]
-        [HttpPost("/aiGenerateTripPlan/{userId}")]
+        [HttpPost("aiGenerateTripPlan/{userId}")]
         public async Task<ActionResult> AiGenerateTripPlan(string userId)
         {
             //674e42c8456b55bec98b2d78
@@ -239,36 +215,33 @@ namespace WayWIthUs_Server.Controllers
             }
 
             List<CityPlan> cityPlans = new List<CityPlan>();
-            
+            for (int i = 0; i < random.Next(1, 5); i++){
                 var cityStartDate = DateTime.Now.AddDays(random.Next(1, 30));
                 var cityEndDate = startDate.AddDays(random.Next(1, 30));
-                var originLocation = await _openAiService.GetOpenAiResponse("write me only some random place name");
-                var originLocationWitoutNull = originLocation == null ? "Tokyo" : originLocation;
+                var originLocationWitoutNull = ApiKeyAndUrl.RandomCities[random.Next(ApiKeyAndUrl.RandomCities.Length)];
 
 
                 List<Accommodation> accommodations = new List<Accommodation>();
-                
+                for (int j = 0; j < random.Next(1, 5); j++){
 
-                    var locationAcc = await _openAiService.GetOpenAiResponse("write me only some random place name");
-                    var ocationAccWitoutNull = locationAcc == null ? "Tokyo" : locationAcc;
+                    var locationAccWitoutNull = ApiKeyAndUrl.RandomCities[random.Next(ApiKeyAndUrl.RandomCities.Length)];
 
                     var accomodation2 = new Accommodation()
                     {
-                        location_acc = ocationAccWitoutNull,
-                        name = await _openAiService.GetOpenAiResponse("pick one accomodation name in city" + originLocationWitoutNull),
-                        description = await _openAiService.GetOpenAiResponse("write some brief description"),
+                        location_acc = locationAccWitoutNull,
+                        name = await _openAiService.GetOpenAiResponse("fill some name for " + originLocationWitoutNull),
+                        description = await _openAiService.GetOpenAiResponse("write some brief description for " + originLocationWitoutNull),
                         image_url = "empty",
                         googleMapUrl = "empty"
                     };
 
                     accommodations.Add(accomodation2);
-                
+                }
 
                 List<Place> places = new List<Place>();
-                
+                for (int j = 0; j < random.Next(1, 5); j++){
 
-                    var loc = await _openAiService.GetOpenAiResponse("write me only some random place name");
-                    var locWitoutNull = loc == null ? "Tokyo" : loc;
+                    var locWitoutNull = ApiKeyAndUrl.RandomCities[random.Next(ApiKeyAndUrl.RandomCities.Length)];
 
                     var place2 = new Place()
                     {
@@ -279,7 +252,7 @@ namespace WayWIthUs_Server.Controllers
                     };
 
                     places.Add(place2);
-                
+                }
 
                 var cityPlan = new CityPlan()
                 {
@@ -290,24 +263,23 @@ namespace WayWIthUs_Server.Controllers
                     Transport = (Transport)random.Next(Enum.GetValues(typeof(Transport)).Length),
                     Accommodations = accommodations,
                     Places = places,
-                    Description = await _openAiService.GetOpenAiResponse("Generate a brief description for a city")
+                    Description = await _openAiService.GetOpenAiResponse("Generate a brief description for a city plan")
                 };
 
 
 
                 cityPlans.Add(cityPlan);
             
-
+            }
 
             var tp = new TripPlan
             {
                 UserId = userId,
                 Participants = new List<string> { userId },
-                Title = await _openAiService.GetOpenAiResponse("Generate a brief title for a trip to New York and Los Angeles"),
+                Title = await _openAiService.GetOpenAiResponse("Generate a brief title for a trip"),
                 Description = await _openAiService.GetOpenAiResponse("Generate a brief description for a trip max 40 words"),
                 StartDate = startDate,
                 EndDate = endDate,
-                /*CityPlans = cityPlans,*/
                 CityPlans = cityPlans,
                 Languages = pickLanguages.ToArray(),
                 Age = new Age { Min = minAge, Max = maxAge },
@@ -349,7 +321,7 @@ namespace WayWIthUs_Server.Controllers
             return CreatedAtAction(nameof(GetById), new { id = tp.Id }, tp);
         }
 
-       
+        
 
         
     }
--- Models/ApiKeyAndUrl.cs
+++ Models/ApiKeyAndUrl.cs
@@ -8,7 +8,7 @@
         public static readonly string USER_INFO = "https://www.googleapis.com/oauth2/v2/userinfo?access_token={token}";
         public static readonly string PLACES_API_TEXT_SEARCH = "https://places.googleapis.com/v1/places:searchText";
         public static readonly string PLACES_API_IMAGE_SEARCH = "https://places.googleapis.com/v1/{name}/media?key={apikey}&{params}";
-        public static readonly string OPEN_KEY = "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";
+        public static readonly string OPEN_KEY = "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";
 
         public static readonly string[] LanguageOptions = new string[]
         {
@@ -37,5 +37,19 @@
             "cruising",
             "educational"
         };
-    };
+
+        public static readonly string[] RandomCities = new string[]
+        {
+            "New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose",
+            "Austin", "Jacksonville", "Fort Worth", "Columbus", "Charlotte", "San Francisco", "Indianapolis", "Seattle", "Denver", "Washington",
+            "Boston", "El Paso", "Nashville", "Detroit", "Oklahoma City", "Portland", "Las Vegas", "Memphis", "Louisville", "Baltimore",
+            "Milwaukee", "Albuquerque", "Tucson", "Fresno", "Sacramento", "Kansas City", "Long Beach", "Mesa", "Atlanta", "Colorado Springs",
+            "Virginia Beach", "Raleigh", "Omaha", "Miami", "Oakland", "Minneapolis", "Tulsa", "Wichita", "New Orleans", "Arlington",
+            "Cleveland", "Bakersfield", "Tampa", "Aurora", "Honolulu", "Anaheim", "Santa Ana", "Corpus Christi", "Riverside", "Lexington",
+            "St. Louis", "Stockton", "Pittsburgh", "Saint Paul", "Cincinnati", "Anchorage", "Henderson", "Greensboro", "Plano", "Newark",
+            "Lincoln", "Toledo", "Orlando", "Chula Vista", "Irvine", "Fort Wayne", "Jersey City", "Durham", "St. Petersburg", "Laredo",
+            "Buffalo", "Madison", "Lubbock", "Chandler", "Scottsdale", "Glendale", "Reno", "Norfolk", "Winston-Salem", "North Las Vegas",
+            "Irving", "Chesapeake", "Gilbert", "Hialeah", "Garland", "Fremont", "Richmond", "Boise", "Baton Rouge", "Des Moines"
+        };
+    }
 }
